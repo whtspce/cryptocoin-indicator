8c916f9574b9f39a7b35dfa5509adc844c9875b8
brkbrk <ixet@openmailbox.org>
2018-01-21 02:46:59 +0400

    Yobit only, new icon

--- cryptocoin-indicator.py
+++ cryptocoin-indicator.py
@@ -11,8 +11,7 @@ from gi.repository import AppIndicator3 as appindicator
 from gi.repository import GObject as gobject
 import signal
 import os
-import urllib
-import json
+import requests
 
 APPINDICATOR_ID = "cryptocoin-indicator"
 
@@ -26,158 +25,39 @@ global currentupdate
 def quit(source):
     gtk.main_quit()
 
+
 ##############################
 # Update Prices
 ##############################
-
-###############
-# BitFinex
-###############
-# Update BTC with BitFinex
-def update_btcusd_bitfinex():
-    url = r"https://api.bitfinex.com/v1/ticker/btcusd"
-    response = urllib.urlopen(url)
-    data = json.loads(response.read())
-    price = data["last_price"]
-    price = str(round(float(price), 3))
-    mystring = "BTCUSD: " + price
-    return mystring
-
-def set_update_btcusd_bitfinex(source):
-    global currentupdate
-    currentupdate = update_btcusd_bitfinex
-
-# Update ETH with BitFinex
-def update_ethbtc_bitfinex():
-    url = r"https://api.bitfinex.com/v1/ticker/ethbtc"
-    response = urllib.urlopen(url)
-    data = json.loads(response.read())
-    price = data["last_price"]
-    price = str(round(float(price), 7))
-    mystring = "ETHBTC: " + price
-    return mystring
-
-def set_update_ethbtc_bitfinex(source):
-    global currentupdate
-    currentupdate = update_ethbtc_bitfinex
-
-def update_ltcusd_bitfinex():
-    url = r"https://api.bitfinex.com/v1/ticker/ltcusd"
-    response = urllib.urlopen(url)
-    data = json.loads(response.read())
-    price = data["last_price"]
-    price = str(round(float(price), 4))
-    mystring = "LTCUSD: " + price
-    return mystring
-
-def set_update_ltcusd_bitfinex(source):
-    global currentupdate
-    currentupdate = update_ltcusd_bitfinex
-
-###############
-# BTC-e
-###############
-def update_btcusd_btce():
-    url = r"https://btc-e.com/api/3/ticker/btc_usd"
-    response = urllib.urlopen(url)
-    data = json.loads(response.read())
+def update_btcusd_yobit():
+    url = r"https://yobit.net/api/3/ticker/btc_usd"
+    response = requests.get(url)
+    data = response.json()
     price = data["btc_usd"]["last"]
     price = str(round(float(price), 3))
     mystring = "BTCUSD: " + price
     return mystring
 
-def set_update_btcusd_btce(source):
+
+def set_update_btcusd_yobit(source):
     global currentupdate
-    currentupdate = update_btcusd_btce
+    currentupdate = update_btcusd_yobit
 
-def update_ethbtc_btce():
-    url = r"https://btc-e.com/api/3/ticker/eth_btc"
-    response = urllib.urlopen(url)
-    data = json.loads(response.read())
-    price = data["eth_btc"]["last"]
-    price = str(round(float(price), 7))
-    mystring = "ETHBTC: " + price
-    return mystring
+# ##############################
+# # Initialization
+# ##############################
 
-def set_update_ethbtc_btce(source):
-    global currentupdate
-    currentupdate = update_ethbtc_btce
 
-def update_ltcusd_btce():
-    url = r"https://btc-e.com/api/3/ticker/ltc_usd"
-    response = urllib.urlopen(url)
-    data = json.loads(response.read())
-    price = data["ltc_usd"]["last"]
-    price = str(round(float(price), 4))
-    mystring = "LTCUSD: " + price
-    return mystring
+# # Default to BTCUSD from Yobit
+currentupdate = update_btcusd_yobit
 
-def set_update_ltcusd_btce(source):
-    global currentupdate
-    currentupdate = update_ltcusd_btce
-
-###############
-# Bitinka
-###############
-# Update BTC with Bitinka
-def update_btcars_bitinka():
-    url = r"https://www.bitinka.com.ar/api/apinka/ticker?format=json"
-    response = urllib.urlopen(url)
-    data = json.loads(response.read())
-    price = data["ARS"]["ask"]
-    price = str(round(float(price), 3))
-    mystring = "BTCARS: " + price
-    return mystring
-
-def set_update_btcars_bitinka(source):
-    global currentupdate
-    currentupdate = update_btcars_bitinka
-
-###############
-# Ripio
-###############
-# Update BTC with Ripio
-def update_btcars_ripio():
-    url = r"https://www.ripio.com/api/v1/rates/"
-    response = urllib.urlopen(url)
-    data = json.loads(response.read())
-    price = data["rates"]["ARS_BUY"]
-    price = str(round(float(price), 2))
-    mystring = "BTCARS: " + price
-    return mystring
-
-def set_update_btcars_ripio(source):
-    global currentupdate
-    currentupdate = update_btcars_ripio
-
-###############
-# SatoshiTango
-###############
-# Update BTC with SatoshiTango
-def update_btcars_satoshitango():
-    url = r"https://api.satoshitango.com/v2/ticker"
-    response = urllib.urlopen(url)
-    data = json.loads(response.read())
-    price = data["data"]["compra"]["arsbtc"]
-    price = str(round(float(price), 3))
-    mystring = "BTCARS: " + price
-    return mystring
-
-def set_update_btcars_satoshitango(source):
-    global currentupdate
-    currentupdate = update_btcars_satoshitango
-
-##############################
-# Initialization
-##############################
-# Default to BTCUSD from BitFinex
-currentupdate = update_btcusd_bitfinex
 
 # Update prices
 def update_price(indicator):
     curprice = currentupdate()
     indicator.set_label(curprice, "BTCUSDX: 1000.32112")
     return True
+
 
 ##############################
 # Create menu
@@ -186,47 +66,9 @@ def build_menu():
     menu = gtk.Menu()
 
     # Which price to display
-    btcusd_bitfinex = gtk.MenuItem("BTCUSD: BitFinex")
-    btcusd_bitfinex.connect("activate", set_update_btcusd_bitfinex)
-    menu.append(btcusd_bitfinex)
-
-    ethbtc_bitfinex = gtk.MenuItem("ETHBTC: BitFinex")
-    ethbtc_bitfinex.connect("activate", set_update_ethbtc_bitfinex)
-    menu.append(ethbtc_bitfinex)
-
-    ltcusd_bitfinex = gtk.MenuItem("LTCUSD: BitFinex")
-    ltcusd_bitfinex.connect("activate", set_update_ltcusd_bitfinex)
-    menu.append(ltcusd_bitfinex)
-
-    # Separator
-    menu.append(gtk.SeparatorMenuItem())
-
-    btcusd_btce = gtk.MenuItem("BTCUSD: BTC-e")
-    btcusd_btce.connect("activate", set_update_btcusd_btce)
-    menu.append(btcusd_btce)
-
-    ethbtc_btce = gtk.MenuItem("ETHBTC: BTC-e")
-    ethbtc_btce.connect("activate", set_update_ethbtc_btce)
-    menu.append(ethbtc_btce)
-
-    ltcusd_btce = gtk.MenuItem("LTCUSD: BTC-e")
-    ltcusd_btce.connect("activate", set_update_ltcusd_btce)
-    menu.append(ltcusd_btce)
-
-    # Separator
-    menu.append(gtk.SeparatorMenuItem())
-
-    btcars_bitinka = gtk.MenuItem("BTCARS: Bitinka")
-    btcars_bitinka.connect("activate", set_update_btcars_bitinka)
-    menu.append(btcars_bitinka)
-
-    btcars_ripio = gtk.MenuItem("BTCARS: Ripio")
-    btcars_ripio.connect("activate", set_update_btcars_ripio)
-    menu.append(btcars_ripio)
-
-    btcars_satoshitango = gtk.MenuItem("BTCARS: SatoshiTango")
-    btcars_satoshitango.connect("activate", set_update_btcars_satoshitango)
-    menu.append(btcars_satoshitango)
+    btcusd_yobit = gtk.MenuItem("BTCUSD: Yobit")
+    btcusd_yobit.connect("activate", set_update_btcusd_yobit)
+    menu.append(btcusd_yobit)
 
     # Separator
     menu.append(gtk.SeparatorMenuItem())
@@ -238,15 +80,18 @@ def build_menu():
     menu.show_all()
     return menu
 
+
 ##############################
 # Main function
 ##############################
 def main():
     # Set up indicator
 
-    indicator = appindicator.Indicator.new(APPINDICATOR_ID,
-                                       os.path.abspath('cryptocoin-indicator.svg'),
-                                       appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator = appindicator.Indicator.new(
+        APPINDICATOR_ID,
+        # os.path.abspath('cryptocoin-indicator.svg'),
+        os.path.abspath('bitcoin-512.png'),
+        appindicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
     indicator.set_menu(build_menu())
 
